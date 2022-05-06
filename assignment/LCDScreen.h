d0fd7096e4e686fcad2e3ce3edddf41fa5bbff7a
daze <leon.baiocchi@studio.unibo.it>
2022-05-06 19:12:20 +0200

    Implement Screen and LCDScreen

---
+++ assignment/LCDScreen.h
@@ -0,0 +1,18 @@
+#ifndef __LCDSCREEN__
+#define __LCDSCREEN__
+
+#include "Screen.h"
+#include <LiquidCrystal.h>
+
+#define MAX_PINS 6
+
+class LCDScreen: public Screen {
+public:
+  LCDScreen(uint8_t rs, uint8_t enable, uint8_t d4, uint8_t d5, uint8_t d6, uint8_t d7);
+  void init();
+  void print(char* msg, int row, int col);
+private:
+  LiquidCrystal* lcd;
+};
+
+#endif
