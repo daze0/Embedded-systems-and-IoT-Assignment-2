2b1053e54b9898aeeae8820e15cb9e3fb556d9ee
FedericoBrunelli <federico.brunelli4@studio.unibo.it>
2022-07-06 19:10:50 +0200

    Add methods and fields to manage products in SmartCoffeeMachine

--- assignment/SmartCoffeeMachine.cpp
+++ assignment/SmartCoffeeMachine.cpp
@@ -1,46 +1,123 @@
 #include "SmartCoffeeMachine.h"
-#define N_MAX 5
+#define N_MAX 10
+
+#define DIM_PROD 4
+#include "Product.h"
+
 
 SmartCoffeeMachine::SmartCoffeeMachine() {
-  this->coffee = N_MAX;
-  this->tea = N_MAX;
-  this->chocolate = N_MAX;
-  this->sugar = N_MAX;
-  this->ready = false;
-  this->make = false;
-  this->assistance = false;
+    this->listOfProducts[0] = new Product("coffee", N_MAX);
+    this->listOfProducts[1] = new Product("tea", N_MAX);
+    this->listOfProducts[2] = new Product("chocolate", N_MAX);
+    this->listOfProducts[3] = new Product("sugar", N_MAX);        
+    this->assistance = false;
+    // Sorry, I know. That's terrible
+    this->productSelected = new Product(this->listOfProducts[0]->getName(), this->listOfProducts[0]->getAvailability());
+    this->nSelfTests = 0;
+    this->mode = "WORKING";//"ASSISTANCE";
+    this->ready = false;
+    this->make = false;
 }
 
 bool SmartCoffeeMachine::productsAvailable() {
-  return (this->coffee == 0) && (this->tea == 0) && (this->chocolate == 0);
+    return (this->listOfProducts[0]->getAvailability() == 0) &&
+        (this->listOfProducts[1]->getAvailability() == 0) &&
+        (this->listOfProducts[2]->getAvailability() == 0);
 }
 
 bool SmartCoffeeMachine::isReady() {
-  return this->ready;
+    return this->ready;
 }
 
 bool SmartCoffeeMachine::isMaking() {
-  return this->make;
+    return this->make;
 }
 
 void SmartCoffeeMachine::setReady(bool ready) {
-  this->ready = ready;
+    this->ready = ready;
+    if (ready == true){
+      this->make = false;
+    }
 }
 
 void SmartCoffeeMachine::setMake(bool make) {
-  this->make = make;
+    this->make = make;
+    if (make == true){
+        this->ready = false;
+    }
 }
 
 void SmartCoffeeMachine::refill() {
-  this->coffee = N_MAX;
-  this->tea = N_MAX;
-  this->chocolate = N_MAX;
+    for (int i = 0; i < DIM_PROD; i++){
+        this->listOfProducts[i]->refill(N_MAX);
+    }
 }
 
 void SmartCoffeeMachine::setAssistance(bool assistance) {
   this->assistance = assistance;
 }
 
-bool SmartCoffeeMachine::IsInAssistanceMode() {
+bool SmartCoffeeMachine::isInAssistanceMode() {
   return this->assistance;
+}
+
+void SmartCoffeeMachine::nextProduct() {
+    //sorry again.
+    if(productSelected == listOfProducts[0]){
+        this->productSelected = listOfProducts[1];
+    } else if(productSelected == listOfProducts[1]){
+        this->productSelected = listOfProducts[2];
+    } else if (productSelected == listOfProducts[2]) {
+        this->productSelected = listOfProducts[3];
+    } else {
+        this->productSelected = listOfProducts[0];
+    }
+}
+
+void SmartCoffeeMachine::previousProduct() {
+      if(productSelected == listOfProducts[0]){
+        this->productSelected = new Product(listOfProducts[3]->getName(), listOfProducts[3]->getAvailability());
+    } else if(productSelected == listOfProducts[1]){
+        this->productSelected = listOfProducts[0];
+    } else if (productSelected == listOfProducts[2]) {
+        this->productSelected = listOfProducts[1];
+    } else {
+        this->productSelected = listOfProducts[2];
+    }
+}
+
+Product* SmartCoffeeMachine::getSelectedProduct(){
+    return this->productSelected;
+}
+
+int SmartCoffeeMachine::getTea() {
+  return this->tea;
+}
+
+int SmartCoffeeMachine::getCoffee() {
+  return this->coffee;
+}
+
+int SmartCoffeeMachine::getChocolate() {
+  return this->chocolate;
+}
+
+int SmartCoffeeMachine::getSugar() {
+  return this->sugar;
+}
+
+int SmartCoffeeMachine::getNSelfTests() {
+  return this->nSelfTests;
+}
+
+void SmartCoffeeMachine::selfTest() {
+  this->nSelfTests++;
+}
+
+void SmartCoffeeMachine::setMode(char* mode) {
+  this->mode = mode;
+}
+
+char* SmartCoffeeMachine::getMode() {
+  return this->mode;
 }
--- assignment/SmartCoffeeMachine.h
+++ assignment/SmartCoffeeMachine.h
@@ -1,25 +1,42 @@
 #ifndef __SMARTCOFFEEMACHINE__
 #define __SMARTCOFFEEMACHINE__
 
+#include "Product.h"
+
 class SmartCoffeeMachine {
+
 public:
-  SmartCoffeeMachine();
-  bool productsAvailable();
-  bool isReady();
-  bool isMaking();
-  void setReady(bool ready);
-  void setMake(bool make);
-  void refill();
-  void setAssistance(bool assistance);
-  bool IsInAssistanceMode();
+    SmartCoffeeMachine();
+    bool productsAvailable();
+    bool isReady();
+    bool isMaking();
+    void setReady(bool ready);
+    void setMake(bool make);
+    void refill();
+    void setAssistance(bool assistance);
+    bool isInAssistanceMode();
+    void nextProduct();
+    void previousProduct();
+    Product* getSelectedProduct();
+    Product* getSugar();
+    Product* getCoffee();
+    Product* getTea();
+    Product* getChocolate();
+    /* getter and increaser for n self tests of machine */
+    int getNSelfTests();
+    void selfTest();
+    /* Setter and getter for current machine mode */
+    void setMode(char* mode);
+    char* getMode();
+
 private:
-  int coffee;
-  int tea;
-  int chocolate;
-  int sugar;
+  Product* listOfProducts[4];
   volatile bool ready;
   volatile bool make;
   volatile bool assistance;
+  Product* productSelected;
+  int nSelfTests;
+  char* mode;
 };
 
 #endif
